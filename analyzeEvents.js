3f96e240c7da3466f0e695f6479163a63641d198
Alex Morales <alexmorales30@gmail.com>
2013-07-06 08:02:24 -0400

    Handling both form submissions and pdf downloads

--- analyzeEvents.js
+++ analyzeEvents.js
@@ -3,33 +3,58 @@
 
       
 	  var defaults = {
-	        elementType: "a",
-            linkType: "pdf",
-            className: "pdf-link"
+            linkClassName: "pdf-link",
+			formClassName: "form-submit",
+			flag: 0
 	  };
 	  
-        
-       // var listen = function(this.elementType){
+
 		$.fn.analyzeEvents = function(options){
 		 
 			var settings = $.extend({ }, defaults, options);
-		 
-			console.log(defaults);
-		 
-            $(this).on('click', function(e){
-              
-                if($(e.target).hasClass(settings.className)){
-                     e.preventDefault();  
-                     
-                     var text = $(e.target).text();
-        
-					console.log(text);
-				};
-                
-            });
-          
-			    return this;
-		  
-        };
+
+			
+				$(this).on('click', function(e)
+				{
+				
+
+					if($(e.target).hasClass(settings.linkClassName))
+					{
+						e.preventDefault();  
+						
+						var link = $(e.target).attr('href').split('/');
+						var filename = link[link.length - 1].split('.');
+
+						_gaq.push(['_trackEvent', 'PDF', 'Download', filename[0]]); 
+						
+					}
+					else if($(e.target).parents().eq(0).hasClass(settings.formClassName))
+					{
+				
+						if(settings.flag != 1 && !$(e.target).parents().eq(1).hasClass('searchBlock')){
+						
+							e.preventDefault();
+		
+							var targetedForm = window.location.href.split('/')[window.location.href.split('/').length - 1];
+							
+							targetedForm = targetedForm.substr(0, targetedForm.indexOf('.'));
+							
+							_gaq.push(['_trackEvent', 'Form', 'Submit', targetedForm]); 
+							
+							settings.flag = 1;
+							setTimeout(function(){
+		
+								$(e.target).click();
+		
+							}, 500);
+	
+						};
+					
+					}
+				})	
+				
+				return this;
+			}
+
     
 }(jQuery));
